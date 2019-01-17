b10bbfcb23ba575539a6c97ab90d2c2354249655
alexeygolovachov <alexeygolovachov@gmail.com>
2019-01-17 14:06:30 +0200

    D result

---
+++ index2.js
@@ -0,0 +1,27 @@
+
+
+// var age=+prompt("vozrast");
+// var summa=+prompt("summa");
+
+// if (age) { 
+//     if (age>=65) {console.log(summa*0.5);}
+
+// else {console.log(summa);}
+
+// }
+// var computeNum = localStorage.getItem("KeycomputerNum");
+// computeNum= computeNum ? +computeNum:Math.round(Math.random()*10);
+// var UserNum=+prompt("try ?");
+// if (UserNum===computeNum) {
+//     alert("win");
+//     localStorage.removeItem("KeycomputerNum")
+// }
+// else {
+//     alert('losser');
+//     localStorage.setItem("KeycomputerNum", computeNum);
+// }
+
+var student = 0;
+student=+prompt();
+student= student ?+ student:Math.round(Math.random()*stydent);
+console.log(student);
---
+++ index3.js
@@ -0,0 +1,29 @@
+var x=0;
+var x1=0;/*peremennye vuvoda*/
+var x2=0;
+var D;
+
+var a=+prompt("a");
+var b=+prompt("b");
+var c=+prompt("c");
+console.log(a);
+console.log(b);
+console.log(c);
+
+D=b*b-4*a*c;
+console.log("D",D);
+// console.log(D**0.5);
+// x=(D>0)? (x1=((-b-D**0.5)/2*a), x2=(-b+D**0.5)/2*a): x=-b/2*a;
+if (D>0) {
+    x1=(-b-D**0.5)/(2*a);
+    x2=(-b+D**0.5)/(2*a);
+    console.log("x1,x2",x1,x2);
+    
+    }
+    else if (D==0) {
+        x=-b/(2*a);
+        console.log("x",x);
+    } 
+    else if (D<0) {console.log("there is no rational results")}
+
+
